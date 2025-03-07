1873a278b13e87219efb549a160a815536c9b310
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2025-03-07 19:12:56 +0800

    stuff

--- sort2sip.py
+++ sort2sip.py
@@ -14,11 +14,12 @@ Device.pin_factory = PiGPIOFactory()
 
 upload_url = "".join([
     "https://detect.roboflow.com/",
-    "paper-and-plastic-detection/4",
+    "green_belt/6",
     "?api_key=",
     API_KEY,
     "&format=json",
-    "&stroke=5"
+    "&stroke=5",
+    "&confidence=30"
 ])
 
 video = cv2.VideoCapture("/dev/video0")
@@ -27,8 +28,32 @@ no_water_btn = Button(5)
 use_water_btn = Button(6)
 water_pump = LED(13)
 
-door_servo = Servo(19)
-rotate_servo = Servo(26)
+no_water_btn_status = 0
+use_water_btn_status = 0
+
+def press_5():
+    global no_water_btn_status
+    no_water_btn_status = 1
+
+def release_5():
+    global no_water_btn_status
+    no_water_btn_status = 0
+
+def press_6():
+    global use_water_btn_status
+    use_water_btn_status = 1
+
+def release_6():
+    global use_water_btn_status
+    use_water_btn_status = 0
+
+use_water_btn.when_pressed = press_6
+use_water_btn.when_released = release_6
+no_water_btn.when_pressed = press_5
+no_water_btn.when_released = release_5
+
+door_servo = Servo(19, min_pulse_width=0.55/1000, max_pulse_width=2.45/1000)
+rotate_servo = Servo(26, min_pulse_width=0.55/1000, max_pulse_width=2.45/1000)
 
 I2C_CHIP = "PCF8574"
 I2C_ADDR = 0x27
@@ -37,6 +62,12 @@ LCD_ROWS = 2
 
 MAX_CCW = 700
 MAX_CW = 2300
+
+PLASTIC_BOTTLE = 5
+TIN_CAN = 2
+PAPER = 4
+
+ACCEPTED = {PLASTIC_BOTTLE, TIN_CAN, PAPER}
 
 class LCD(CharLCD):
     _lock = threading.Lock()
@@ -68,6 +99,8 @@ kill2_event = threading.Event()
 def main():
     while not video.isOpened():
         pass
+
+    door_servo.min()
 
     welcome_thread = threading.Thread(
         target=print_scroll,
@@ -102,25 +135,24 @@ def main():
 
         scroll1_event.clear()
 
-       	angle = 0
-        if n == 1:
+        if n == PLASTIC_BOTTLE:
             lcd1.print("Detected:\r\nPlastic bottle")
             points += 70 
             rotate_servo.min()
-        elif n == 1:
+        elif n == TIN_CAN:
             lcd1.print("Detected:\r\nAluminum can")
             points += 135
             rotate_servo.mid()
-        elif n == 2:
+        elif n == PAPER:
             lcd1.print("Detected:\r\nPaper")
             points += 10
             rotate_servo.max()
 
         lcd2.print(f"Points: {points} mL")
-        time.sleep(0.5)
+        time.sleep(5)
 
-        door_servo.max()
-        time.sleep(0.5)
+        door_servo.mid()
+        time.sleep(1.5)
 
         door_servo.min()
         time.sleep(0.5)
@@ -135,8 +167,7 @@ def main():
         scroll2_event.clear()
 
         if water:
-            run_motor(points)
-            points = 0
+            points = run_motor(points)
         
 
 def print_scroll(lcd, message, scroll_event, kill_event):
@@ -168,18 +199,26 @@ def map_degrees_to_servo(ccw, cw, deg):
 
 def wait_for_trash():
     data = infer()
-    print(data)
-    if len(data['predictions']) > 0:
-        return data['predictions'][0]['class_id']
+    pred = data['predictions']
+    print(pred)
+    if len(pred) > 0:
+        first = pred[0]['class_id']
+        if first in ACCEPTED:
+            return first
+
     return wait_for_trash()
 
 
 def wait_for_button_press():
     while True:
-        if use_water_btn.is_pressed: 
+        if use_water_btn_status == 1: 
+            while not use_water_btn_status == 0:
+                pass
             print("Use water")
             return True
-        elif no_water_btn.is_pressed:
+        elif no_water_btn_status == 1:
+            while not no_water_btn_status == 0:
+                pass
             print("DO NOT use water")
             return False
         
@@ -190,7 +229,7 @@ def infer():
 
     # Resize (while maintaining the aspect ratio) to improve speed and save bandwidth
     height, width, channels = img.shape
-    scale = 416 / max(height, width)
+    scale = 640 / max(height, width)
     img = cv2.resize(img, (round(scale * width), round(scale * height)))
 
     # Encode image to base64 string
@@ -208,8 +247,16 @@ def infer():
 
 def run_motor(points):
     water_pump.on()
-    time.sleep(points / 20)
+    for i in range(points):
+        if use_water_btn_status == 1: 
+            while not use_water_btn_status == 0:
+                pass
+            break
+        points -= 1        
+        lcd2.print(f"Points: {points} mL")
+        time.sleep(0.02)
     water_pump.off()
+    return points
 
 
 if __name__ == "__main__":
